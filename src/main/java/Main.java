c73c6d2b9ef436c49943a2b81ea942ab8707a8e9
tubai <tubai@172.19.148.23>
2023-07-19 09:43:24 +0300

    Main+Owner push

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,8 @@
-
-import javax.swing.*;
 import java.util.*;
+import Owner.*;
 
 public class Main {
-
+    private ArrayList<Owner> owners;
     public static void confirmPassword(String password){
         Scanner scan = new Scanner(System.in);
         System.out.print("\nPlease confirm your password: ");
@@ -15,7 +14,19 @@ public class Main {
             System.out.println();
         }
     }
-    public static void printLogin (int loginType){
+    public static void printLogin (){
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("Please enter your username: ");
+        String username = scan.nextLine();
+
+        System.out.print("\nPlease enter your password: ");
+        String password = scan.nextLine();
+
+        System.out.print("\n");
+    }
+
+    public static void printRegistration(int registrationType,Owner owner){
         Scanner scan = new Scanner(System.in);
 
         System.out.print("Please enter your username: ");
@@ -38,28 +49,30 @@ public class Main {
             password = scan.nextLine();
             passArray = password.toCharArray();
         } // password shouldn't be less than 8 chars
+        confirmPassword(password);
+        if(registrationType==4){
 
-        if(loginType==4) {
-          confirmPassword(password);
+        } else if (registrationType==5) {
+
+            owner.setUserName(username);
+            owner.setPassword(password);
         }
 
-        if(loginType==1||loginType==2||loginType==3){
-            
-        }
-        System.out.print("\n");
     }
 
+    public static void owners(ArrayList<String> owners){
+
+    }
 
 
 
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-//        ArrayList<String> tenantsUsername;
-//        ArrayList<String>  tenantsPassword;
-        System.out.println("**************  welcome to sacanati project **************");
-        System.out.print("\n Please choose and press the type of login: \n 1)admin 2)tenant 3)owner " +
-                "4)register as new tenant : ");
+        ArrayList<Owner> owners= new ArrayList<Owner>();
+        System.out.println("\n**************  welcome to sacanati project **************");
+        System.out.print("\n Please choose and press the type of login: \n 1)admin 2)owner 3)tenant " +
+                "4)register as new tenant 5)register as an owner : ");
         int logTypeNum=0;
         boolean flag=false;
 
@@ -68,45 +81,40 @@ public class Main {
 
                 logTypeNum = scanner.nextInt();
 
-                if (logTypeNum > 0 && logTypeNum < 5)
+                if (logTypeNum > 0 && logTypeNum < 6)
                     flag = true;
                 else
-                    System.out.print("the number shouldn't exceed this range (1-4) please try again: ");
+                    System.out.print("the number shouldn't exceed this range (1-5) please try again: ");
 
 
             } catch (Exception exp) {
-                System.out.print("Please enter only a number from one to three: ");
+                System.out.print("Please enter only a number from one to 5: ");
                 scanner.next();
             }
         }
-        if(logTypeNum==1){
-            printLogin(logTypeNum);
+        if(logTypeNum==1){ // admin
+            printLogin();
 
         }
-        if(logTypeNum==2){
-            printLogin(logTypeNum);
+        if(logTypeNum==2){ // owner
+            printLogin();
 
         }
-        if(logTypeNum==3){
-            printLogin(logTypeNum);
-
-        }
-
-        if(logTypeNum==4){
-            printLogin(logTypeNum);
+        if(logTypeNum==3){ // tenant
+            printLogin();
 
         }
 
+        if(logTypeNum==4){ // registration as tenant
+            printRegistration(4,null);
 
-
-
-
-
-
-
-
-
-
+        }
+        if(logTypeNum==5){ // registration as owner
+            Owner owner = new Owner();
+            printRegistration(5,owner);
+            owners.add(owner);
+        }
+        
 
     }
 }
---
+++ src/main/java/Owner/Owner.java
@@ -0,0 +1,34 @@
+package Owner;
+import java.util.ArrayList;
+
+public class Owner {
+	
+	private ArrayList<String> photosLink;
+	private String userName;
+	private String password;
+	private String information;
+	    public Owner(){
+
+	    }
+	    public void addPhoto(String photo){
+	        photosLink.add(photo);
+	    }
+	    public void setInformation(String info){
+	        information = info;
+	    }
+	    public void setUserName(String username){
+	        this.userName = username;
+	    }
+	    public void setPassword(String password){
+	        this.password = password;
+	    }
+	    public String getUserName(){
+	        return userName;
+	    }
+	    public String getPassword(){
+	        return password;
+	    }
+
+
+
+	}
